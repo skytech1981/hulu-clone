92c539cd30a65fcec3cb85d0868fe1b5a4a49337
sali itzhaki <sali1943@hotmail.com>
2020-10-11 17:29:44 +0300

    adding nav component

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './nav.css'
+
+
+function Nav() {
+    return (
+        <div className="navbar">
+           <h1> this is navbar</h1> 
+        </div>
+    )
+}
+
+export default Nav
