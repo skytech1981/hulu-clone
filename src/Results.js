8deeb9833bf562146aa2832e7649cd70af2cde2f
sali itzhaki <sali1943@hotmail.com>
2020-10-12 12:19:23 +0300

    connecting axios to tmdb database

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React ,{useEffect, useState} from 'react'
 import './results.css';
 import VideoCard from "./VideoCard";
+import axios from "./axios";
+import requests from "./requests";
+
 
 function Results() {
+    const [movies ,setMovies] = useState([]);
+    
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchActionMovies);
+                setMovies(request.data.results);
+                console.log(request)
+                return request;
+                }
+
+            fetchData();
+    },[])
+
     return (
         <div className="results">
-            <h1>Reeeeesults</h1>
-            <VideoCard/>
+            {movies.map((movie) => (
+                <VideoCard movie={movie}/>
+            ))} 
+           
         </div>
     )
 }
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import "./videocard.css";
+import TextTruncate from "react-text-truncate";
 
-function VideoCard() {
+const base_url= "https://image.tmdb.org/t/p/original/";
+
+function VideoCard({movie}) {
+
     return (
-        <div className="videocard">
-            <h2>videocard</h2>
+        <div className="videoCard">
+            
+            <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
+             alt=""/>
+             <TextTruncate
+             line={2}
+             element="p"
+             truncateText="..."
+            //  textTruncateChild={<a href="#">Read on</a>}
+             text={movie.overview}
+             
+             />
+           
+            <h1>{movie.title || movie.original.title}</h1>
+            <p>{movie.vote_count}</p>
+
         </div>
     )
 }
